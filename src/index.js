cdd8515e0bcf64dd325ca116386bf4af1848ceaa
Diana Grayden <graydendi@mail.ru>
2024-08-05 11:41:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsMap = new Map(bracketsConfig);
+  let openBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  let closeBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+
+  for (let char of str) {
+    if(openBrackets.has(char)) {
+      if (closeBrackets.has(char) && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      if (stack.length === 0 || bracketsMap.get(stack.pop()) !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
